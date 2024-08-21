56d2edef952c9488e49cbb73b63e43b674db2ab4
qsqcqs <qsqcqs@gmail.com>
2024-08-21 14:56:33 -0700

    port to c

---
+++ terraria.c
@@ -0,0 +1,231 @@
+#include <string.h>
+#include<stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+//#include <curses.h>
+int fileread(char file[],long bytenum)
+{
+FILE* fptr;
+fptr = fopen(file,"rb");
+int a = 0;
+unsigned char b[1];
+    fseek(fptr,bytenum,SEEK_SET);
+	fread(b,1,1,fptr);
+//	printf("%x",b[0]);
+    return(b[0]);
+fclose(fptr);
+//printf("\n");
+}
+void filewrite(char file[],unsigned char input[])
+{
+
+//b=&a;
+FILE *fptr;
+fptr = fopen(file,"wb");
+fprintf(fptr,"%c",input);
+fclose(fptr);
+}
+void fileappend(char file[],unsigned char input)
+{
+    FILE *fptr;
+    fptr=fopen(file,"ab");
+    fprintf(fptr,"%c",input);
+    fclose(fptr);
+}
+char* chunkgen(long x,long y)
+{
+	char *out = malloc(140);
+	
+	unsigned long buffer;
+	buffer = x;
+	
+	out[0]=buffer/256;
+	buffer=buffer-(buffer/256*256);
+	out[1]=buffer/256;
+	buffer=buffer-(buffer/256*256);
+	out[2]=buffer/256;
+	buffer=buffer-(buffer/256*256);
+	out[3]=buffer/256;
+	buffer=buffer-(buffer/256*256);
+	out[4]=buffer/256;
+	buffer=buffer-(buffer/256*256);
+	out[5]=buffer/255;
+	buffer=buffer-(buffer/256*256);
+	out[6]=buffer/256;
+	buffer=buffer-(buffer/256*256);
+	out[7]=buffer;
+	buffer=buffer-(buffer/256*256);
+	buffer = y;
+	out[1]=buffer/256;
+	buffer=buffer-(buffer/256*256);
+	out[2]=buffer/256;
+	buffer=buffer-(buffer/256*256);
+	out[3]=buffer/256;
+	buffer=buffer-(buffer/256*256);
+	out[4]=buffer/256;
+	buffer=buffer-(buffer/256*256);
+	out[5]=buffer/255;
+	buffer=buffer-(buffer/256*256);
+	out[6]=buffer/256;
+	buffer=buffer-(buffer/256*256);
+	out[7]=buffer;
+	buffer=0;
+	while (buffer<128)
+	{
+		buffer++;
+		//temp because I cant think of a gen function
+		out[buffer+16]='f';
+	}	
+	return(out);
+}
+char* pixrender(char a)
+{
+	if (a == 'f')
+        {
+			return("\x1b[32;45mXX");
+        }
+
+    else
+        {
+			printf("block type %c not found", a);
+            exit(1);
+        }
+}
+void game()
+{
+//spawncoords 00 idk for now
+	int spawnx = 0;
+	int spawny = 0;
+	char* chunktl[140];
+	char* chunktm[140];
+	char* chunktr[140];
+	char* chunkml[140];
+	char* chunkmm[140];
+	char* chunkmr[140];
+	char* chunkbl[140];
+	char* chunkbm[140];
+	char* chunkbr[140];
+	int chunkspawnx = spawnx/16;
+	int chunkspawny = spawnx/16;
+	*chunktl = chunkgen(chunkspawnx-1,chunkspawny+1);
+	*chunktm = chunkgen(chunkspawnx,chunkspawny+1);
+	*chunktr = chunkgen(chunkspawnx+1,chunkspawny+1);
+	*chunkml = chunkgen(chunkspawnx-1,chunkspawny);
+	*chunkmm = chunkgen(chunkspawnx,chunkspawny);
+	*chunkmr = chunkgen(chunkspawnx+1,chunkspawny);
+	*chunkbl = chunkgen(chunkspawnx-1,chunkspawny-1);
+	*chunkbm = chunkgen(chunkspawnx,chunkspawny-1);
+	*chunkbr = chunkgen(chunkspawnx+1,chunkspawny-1);
+	bool c = true;
+	int playerx=spawnx;
+	int playery=spawny;
+	int xrend;
+	int yrend;
+	int relx;
+	int rely;
+	char xoff;
+	char yoff;
+	char input;
+	while (c)
+	{
+		int yrend=9;
+		
+		while (yrend >-9)
+		{
+			xrend = -16;
+			while (xrend < 16)
+			{
+				//printf("%dx,%dy",xrend,yrend);	
+				xoff = 'm';
+				yoff = 'm';
+				relx = playerx + xrend;
+				rely = playery + yrend;
+				if (relx > 8)
+				{
+					xoff = 'r';
+					relx=relx-16;
+				}		
+				if (relx < -7)
+				{
+					xoff = 'l';
+					relx = relx+16;
+				}			
+				if (rely > 8)
+				{
+					yoff = 't';
+					rely=rely-16;
+				}		
+				if (rely < -7)
+				{
+					yoff = 'b';
+					rely = rely+16;
+				}
+				int arrayloc = relx + 7 +(8-rely)*16;
+				if (yoff == 't')
+					{
+					if (xoff == 'l')
+						{
+						pixrender(chunktl[arrayloc]);
+						}  
+					if (xoff == 'm')
+						{
+						pixrender(chunktm[arrayloc]);
+						}  
+					if (xoff == 'r')
+						{
+						pixrender(chunktr[arrayloc]);
+						}  
+					}
+				if (yoff == 'm')
+					{
+					if (xoff == 'l')
+						{
+						pixrender(chunkml[arrayloc]);
+						}  
+					if (xoff == 'm')
+						{
+						pixrender(chunkmm[arrayloc]);
+						}  
+					if (xoff == 'r')
+						{
+						pixrender(chunkmr[arrayloc]);
+						}  
+					}
+				if (yoff == 'b')
+					{
+					if (xoff == 'l')
+						{
+						pixrender(chunkbl[arrayloc]);
+						}  
+					if (xoff == 'm')
+						{
+						pixrender(chunkbm[arrayloc]);
+						}  
+					if (xoff == 'r')
+						{
+						pixrender(chunkbr[arrayloc]);
+						}  
+					}
+				printf("\n");
+				xrend++;
+			}
+			yrend=yrend-1;
+		}
+		printf("> ");
+		scanf("%c", &input);
+		if (input == 'q')
+		{
+			c=false;
+		}
+	}
+}
+int main()
+{
+//printf("\033[32;45mXX\n\033[0m");
+game();
+//printf(pixrender('a'));
+printf("\x1b[39;49m");
+printf("\n");
+return(0);
+
+}
